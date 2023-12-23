baad90ba3ba04514826ad90fe4edd301a4026ad7
190204987 <elegant.tent4002@fastmail.com>
2023-12-23 22:05:49 +0000

    First commit

---
+++ R/stanagedepth-package.R
@@ -0,0 +1,16 @@
+#' The 'stanagedepth' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name stanagedepth-package
+#' @aliases stanagedepth
+#' @useDynLib stanagedepth, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version NA. https://mc-stan.org
+#'
+NULL
